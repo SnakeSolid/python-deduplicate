2b0bb7bbede03b9762f4d629a2ce605f6f06e508
Shabanov Anton <snakesolid@ngs.ru>
2014-10-19 13:16:55 +0700

    + similar files detection.

--- __main__.py
+++ __main__.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import hashlib
 import os
+import collections
 
 
 class Deduplicator:
+	BLOCK_SIZE = 4096
+	
 	def __init__(self, files, min_size):
 		self.files = files
 		self.min_size = min_size
@@ -17,11 +21,44 @@ class Deduplicator:
 			for listed in os.listdir(path):
 				self.walk(os.path.join(path, listed), append)
 	
+	def fingerprint(self, path):
+		with open(path) as reader:
+			hasher_md5 = hashlib.md5()
+			hasher_sha256 = hashlib.sha256()
+			
+			while True:
+				data_buffer = reader.read(self.BLOCK_SIZE)
+
+				if len(data_buffer) == 0:
+					break
+				
+				hasher_md5.update(data_buffer)
+				hasher_sha256.update(data_buffer)
+				
+			return { "path": path, "size": os.path.getsize(path), "md5": hasher_md5.hexdigest(), "sha256": hasher_sha256.hexdigest() }
+		return None
+	
 	def process(self):
-		files = []
+		all_files = []
 		
 		for path in self.files:
-			self.walk(os.path.expanduser(path), files.append)
+			self.walk(os.path.expanduser(path), all_files.append)
+		
+		choosen_files = (listed for listed in all_files if os.path.getsize(listed) > self.min_size)
+		hashed_files = (self.fingerprint(listed) for listed in choosen_files)
+		collected_files = collections.defaultdict(list)
+		
+		for listed in hashed_files:
+			path = listed["path"]
+			size = listed["size"]
+			digest_md5 = listed["md5"]
+			digest_sha256 = listed["sha256"]
+			
+			key = "{0}:{1}:{2}".format(size, digest_md5, digest_sha256)
+			
+			collected_files[key].append(path)
+		
+		similar_files = (listed for _, listed in collected_files.iteritems() if len(listed) > 1)
 		
 		pass
 
